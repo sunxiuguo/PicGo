1972b0a5bd3d3037cae25ad8e084172ad535a2c0
sunxg <sunxg@fenbi.com>
2019-10-23 19:43:18 +0800

    MOD: 更改图片链接

--- docs/APP.vue
+++ docs/APP.vue
@@ -2,21 +2,16 @@
   #app(v-cloak)
     #header
       .mask
-      img.logo(src="~icons/256x256.png", alt="PicGo")
-      h1.title PicGo
-        small(v-if="version") {{ version }}
-      h2.desc 图片上传+管理新体验
-      button.download(@click="goLink('https://github.com/Molunerfinn/picgo/releases')") 免费下载
-      button.download(@click="goLink('https://picgo.github.io/PicGo-Doc/zh/guide/')") 查看文档
+      img.logo(src="~icons/icon.svg", alt="VisualClipboard")
+      h1.title VisualClipboard
+      h2.desc 优雅的剪贴板可视化管理
+      button.download(@click="goLink('https://github.com/sunxiuguo')") 免费下载
+      button.download(@click="goLink('https://github.com/sunxiuguo')") 查看文档
       h3.desc
-        | 基于#[a(href="https://github.com/SimulatedGREG/electron-vue" target="_blank") electron-vue]开发
-      h3.desc
-        | 支持macOS,Windows,Linux
-      h3.desc
-        | 支持#[a(href="https://picgo.github.io/PicGo-Doc/zh/guide/config.html#%E6%8F%92%E4%BB%B6%E8%AE%BE%E7%BD%AE%EF%BC%88v2-0%EF%BC%89" target="_blank") 插件系统]，让PicGo更强大
+        | 支持macOS,Windows
     #container.container-fluid
       .row.ex-width
-        img.gallery.col-xs-10.col-xs-offset-1.col-md-offset-2.col-md-8(src="https://cdn.jsdelivr.net/gh/Molunerfinn/test/picgo-site/first.png")
+        img.gallery.col-xs-10.col-xs-offset-1.col-md-offset-2.col-md-8(src="http://cdn.zhusun.club/image/clipboard/12.png")
       .row.ex-width.display-list
         .display-list__item(v-for="(item, index) in itemList" :key="index" :class="{ 'o-item': index % 2 !== 0 }")
           .col-xs-10.col-xs-offset-1.col-md-7.col-md-offset-0
@@ -26,60 +21,45 @@
             .display-list__desc {{ item.desc }}
       .row.ex-width.info
         .col-xs-10.col-xs-offset-1
-          | &copy;2017 - {{ year }} #[a(href="https://github.com/Molunerfinn" target="_blank") Molunerfinn]
+          | &copy;2019 - {{ year }} #[a(href="https://github.com/sunxiuguo" target="_blank") sunxiuguo]
 </template>
 <script>
 export default {
-  name: '',
   data () {
     return {
-      version: '',
       year: new Date().getFullYear(),
       itemList: [
         {
-          url: 'https://cdn.jsdelivr.net/gh/Molunerfinn/test/picgo-site/second.png',
-          title: '精致设计',
-          desc: 'macOS系统下，支持拖拽至menubar图标实现上传。menubar app 窗口显示最新上传的5张图片以及剪贴板里的图片。点击图片自动将上传的链接复制到剪贴板。（Windows平台不支持）'
+          url: 'http://cdn.zhusun.club/image/clipboard/1.png',
+          title: '优雅设计',
+          desc: '支持复制多种格式，保留复制内容的所有样式，内容丰富，内容过多时可以滚动查看，方便查找您的剪贴板记录。'
         },
         {
-          url: 'https://cdn.jsdelivr.net/gh/Molunerfinn/test/picgo-site/third.png',
-          title: 'Mini小窗',
-          desc: 'Windows以及Linux系统下提供一个mini悬浮窗用于用户拖拽上传，节约你宝贵的桌面空间。'
+          url: 'http://cdn.zhusun.club/image/clipboard/4.png',
+          title: '元模块',
+          desc: '每个剪贴板记录的元模块，包含顶部tabbar，中间的内容区域展示原格式内容，下方记录您复制此条内容的时间，也给您提供了复制按钮，方便再次复制。'
         },
         {
-          url: 'https://cdn.jsdelivr.net/gh/Molunerfinn/test/picgo-site/forth.png',
-          title: '便捷管理',
-          desc: '查看你的上传记录，重复使用更方便。支持点击图片大图查看。支持删除图片（仅本地记录），让界面更加干净。'
+          url: 'http://cdn.zhusun.club/image/clipboard/5.png',
+          title: '图片记录',
+          desc: '支持记录剪贴板图片记录，无论是截屏还是复制图片，都会记录在我们的剪贴板里，列表展示图片缩略图，方便快速查找。'
         },
         {
-          url: 'https://cdn.jsdelivr.net/gh/Molunerfinn/test/picgo-site/fifth.png',
-          title: '可选图床',
-          desc: '默认支持微博图床、七牛图床、腾讯云COS、又拍云、GitHub、SM.MS、阿里云OSS、Imgur。方便不同图床的上传需求。2.0版本开始更可以自己开发插件实现其他图床的上传需求。'
+          url: 'http://cdn.zhusun.club/image/clipboard/6.png',
+          title: '大图预览',
+          desc: '点击列表缩略图，可以预览更高画质的图片记录。'
         },
         {
-          url: 'https://cdn.jsdelivr.net/gh/Molunerfinn/test/picgo-site/sixth.png',
-          title: '多样链接',
-          desc: '支持5种默认剪贴板链接格式，包括一种自定义格式，让你的文本编辑游刃有余。'
-        },
-        {
-          url: 'https://cdn.jsdelivr.net/gh/Molunerfinn/test/picgo-site/seventh.png',
-          title: '插件系统',
-          desc: '2.0版本开始支持插件系统，让PicGo发挥无限潜能，成为一个极致的效率工具。'
+          url: 'http://cdn.zhusun.club/image/clipboard/3.png',
+          title: '关键字搜索',
+          desc: '右上角预留了关键字搜索栏，可以在此输入关键字，方便快捷的搜索到目标记录。'
         }
       ]
     }
   },
-  created () {
-    this.getVersion()
-  },
   methods: {
     goLink (link) {
       window.open(link, '_blank')
-    },
-    async getVersion () {
-      const release = 'https://api.github.com/repos/Molunerfinn/PicGo/releases/latest'
-      const res = await this.$http.get(release)
-      this.version = res.data.name
     }
   }
 }
@@ -95,6 +75,8 @@ h1
   margin 0
   padding 0
   font-family "Source Sans Pro","Helvetica Neue","PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif  
+html
+    background #f5f5f5
 #app
   position relative
 .mask
@@ -108,7 +90,7 @@ h1
 #header
   height 100vh
   width 100%
-  background-image url("https://cdn.jsdelivr.net/gh/Molunerfinn/test/picgo-site/bg.jpeg")
+  background-image url("http://cdn.zhusun.club/image/clipboard/backg.jpeg")
   background-attachment fixed
   background-size cover
   background-position center
@@ -176,7 +158,7 @@ h1
       background #2E2E2E
       overflow hidden
       &.o-item
-        background #fff
+        background #f5f5f5
         .display-list__desc
           color #2E2E2E
       img
@@ -194,7 +176,7 @@ h1
       color #fff
       margin-top 20px
   .info
-    padding 48px 0
+    padding 24px 0
     background #2E2E2E
     color #fff
     a
